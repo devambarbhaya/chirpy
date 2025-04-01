a47d8bfc5149e23c5c9258871b3172824236db63
devambarbhaya <barbhaya.devam@gmail.com>
2025-04-01 06:22:36 +0000

    setup server

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Handler: mux,
+		Addr: ":8080",
+	}
+	log.Println("Server starting on :8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Server error: ", err)
+	}
+}
